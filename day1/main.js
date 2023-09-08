d723aa071d536b0f61c92bfea0c28bbcb49026bf
Carlos Hagge <carloshaggecunha@gmail.com>
2023-09-07 22:52:06 -0400

    js function to read input

--- day1/main.js
+++ day1/main.js
@@ -1,5 +1,16 @@
+const fs = require('fs');
+
+function readInputTxt() {
+    try {
+        const data = fs.readFileSync('./input.txt', 'utf8');
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
 function main() {
-    console.log("main");
+    console.log(readInputTxt());
 }
 
 if (require.main === module) {
