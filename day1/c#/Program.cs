5a567db3a8dceacf48ee5890aa9b368323f9eded
Carlos Hagge <carloshaggecunha@gmail.com>
2023-09-14 23:25:44 -0400

    add day 1 solution in c#, update gitignore for c#

---
+++ day1/c#/Program.cs
@@ -0,0 +1,24 @@
+﻿var log = (object msg) => Console.WriteLine(msg);
+
+string ReadInputTxt() {
+    var contents = File.ReadAllText("../input.txt");
+    if (contents is null) {
+        throw new FileNotFoundException("Error reading input file");
+    }
+    return contents;
+}
+
+var input = ReadInputTxt();
+var elves = input.Trim().Split("\n\n");
+var cals = new List<int>();
+foreach (var elf in elves) {
+    var elfCals = 0;
+    foreach (var foodItem in elf.Trim().Split("\n")) {
+        var foodCals = int.Parse(foodItem);
+        elfCals += foodCals;
+    }
+    cals.Add(elfCals);
+}
+cals.Sort();
+
+log(cals.Last());
